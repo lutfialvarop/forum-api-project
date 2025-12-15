d262fa8da18a252e474a545781f80be0e32e6b09
Lutfi Alvaro Pratama <78428836+lutfialvarop@users.noreply.github.com>
2025-12-15 19:20:22 +0700

    feat: add use case for like

--- src/Applications/use_case/GetThreadDetailUseCase.js
+++ src/Applications/use_case/GetThreadDetailUseCase.js
@@ -1,16 +1,24 @@
 const ThreadDetail = require('../../Domains/threads/entities/ThreadDetail');
 
 class GetThreadDetailUseCase {
-    constructor({ threadRepository, commentRepository, replyRepository }) {
+    constructor({ threadRepository, commentRepository, replyRepository, commentLikeRepository }) {
         this._threadRepository = threadRepository;
         this._commentRepository = commentRepository;
         this._replyRepository = replyRepository;
+        this._commentLikeRepository = commentLikeRepository;
     }
 
     async execute(threadId) {
         await this._threadRepository.verifyThreadAvailability(threadId);
         const thread = await this._threadRepository.getThreadById(threadId);
         const comments = await this._commentRepository.getCommentsByThreadId(threadId);
+
+        const commentIds = comments.map((comment) => comment.id);
+
+        let likeCounts = [];
+        if (this._commentLikeRepository) {
+            likeCounts = await this._commentLikeRepository.getLikeCountsByCommentIds(commentIds);
+        }
 
         const mappedComments = await Promise.all(
             comments.map(async (comment) => {
@@ -26,12 +34,16 @@ class GetThreadDetailUseCase {
                     }));
                 }
 
+                const likeCountItem = likeCounts.find((item) => item.comment_id === comment.id);
+                const likeCount = likeCountItem ? Number(likeCountItem.count) : 0;
+
                 return {
                     id: comment.id,
                     username: comment.username,
                     date: comment.date,
                     content: comment.is_delete ? '**komentar telah dihapus**' : comment.content,
                     replies,
+                    likeCount,
                 };
             })
         );
